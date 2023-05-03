fac6f59ed9728ba35de02586469554a7f2f65c31
LiamSirBean <122816237+LiamSirBean@users.noreply.github.com>
2023-05-03 10:30:20 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bradenisgoofy.onrender.com/bare/',
+    bare: 'https://ultraviolet.liamsirbean.repl.co/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
