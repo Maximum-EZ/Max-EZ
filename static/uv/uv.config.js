8b364a34c738b8bdbbb469522ddac594ccbf784c
LiamSirBean <rfwrl13@gmail.com>
2023-05-03 18:33:09 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://roblox.com',
+    bare: 'https://max-ez-axrz.onrender.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
